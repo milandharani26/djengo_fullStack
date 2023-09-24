100e83cfd61f5751a52397689222e5b9c280aac5
milandharani26 <113498278+milandharani26@users.noreply.github.com>
2023-09-24 18:37:48 +0530

    first commit

---
+++ servies/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Servies(models.Model):
+    servies_icon=models.FileField(upload_to="project/", max_length=300,null=True,default=None)
+    servies_title=models.CharField(max_length=50)
+    servies_des = models.TextField()
